4e50e7a5e08094df0f18176e920af2a16b6d26bd
ASahithi2005 <adlasahithi8@gmail.com>
2025-01-01 19:43:22 +0530

    add

---
+++ WinnerOfCircularGame.java
@@ -0,0 +1,18 @@
+// Time Complexity : O(n^2)
+
+import java.util.*;
+
+class Solution {
+    public int findTheWinner(int n, int k) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            arr.add(i);
+        }
+        int index = 0;
+        while (arr.size() > 1) {
+            index = (index + k - 1) % arr.size();
+            arr.remove(index);
+        }
+        return arr.get(0);
+    }
+}
